0ec70f5f8a874e79139fbdc28b9a81574f8f731c
TMsudarsan <sudarsantm10@gmail.com>
2025-11-14 16:17:44 +0530

    minor change

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,18 +7,18 @@ export  const protectRoute = [
         try {
             const clerkId = req.auth().userId
 
-            if(!clerkId) return res.status(401).json({mes:"Unauthorized - invaild token"})
+            if(!clerkId) return res.status(401).json({message:"Unauthorized - invaild token"})
 
             const user = await User.findOne({clerkId})
 
-            if(!user) return  res.status(404).json({msg:"User not found"})
+            if(!user) return  res.status(404).json({message:"User not found"})
 
                 req.user = user
 
                 next()
         } catch (error) {
             console.error("Error in protectRoute",error.message);
-            res.status(500).json({msg:"internal server error"})
+            res.status(500).json({message:"internal server error"})
             
         }
     }
